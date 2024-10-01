ef3acaa11a760a541b0f78700916bb11bdc2a499
Carlos Silva <cvcardos5@gmail.com>
2024-09-30 23:33:17 -0300

    simplificando automato

--- automato_finito.py
+++ automato_finito.py
@@ -4,17 +4,11 @@ from validadores import (
     checa_eh_subconjunto,
 )
 from dataclasses import dataclass
-from typing import Set, TypeAlias, cast, Dict
+from typing import Set, TypeAlias, Dict
 from enum import Enum
+import sys
 
 Estado: TypeAlias = Enum
-
-
-@dataclass(frozen=True, eq=True)
-class Transicao:
-    estado_inicial: Estado
-    simbolo: str
-    estado_final: Estado
 
 
 @dataclass
@@ -24,31 +18,78 @@ class AutomatoFinitoDeterministico:
     funcao_de_transicao: Dict[Estado, Dict[str, Estado]]
     estado_inicial: Estado
     estados_finais: Set[Estado]
+    verbose: bool = False
+    interactive: bool = False
 
     def __post_init__(self) -> None:
         self.estado_atual = self.estado_inicial
         self.__checa_eh_valido()
 
-    def valida_cadeia(self, cadeia: str, verbose: bool = False) -> bool:
+    def valida_cadeia(self, cadeia: str) -> bool:
         if cadeia is None:
             raise Exception("Defina uma cadeia (vazia ou não) para validar")
 
         estado_atual: Estado = self.estado_inicial
         cursor: int = 0
-        while True:
-            prox_char: str = cadeia[cursor] if len(cadeia) > 0 else ""
+        while cursor < len(cadeia):
+            self.__print_config(cadeia, cursor, estado_atual)
+
+            char: str = cadeia[cursor]
 
             if estado_atual not in self.funcao_de_transicao:
-                
-            prox_estado: Estado = self.funcao_de_transicao[estado_atual][prox_char]
+                self.__print_config(
+                    cadeia,
+                    cursor,
+                    estado_atual,
+                    f"Cadeia rejeitada. Sem transições disponíveis no estado {estado_atual}",
+                )
+                return False
 
+            if char not in self.funcao_de_transicao[estado_atual]:
+                self.__print_config(
+                    cadeia,
+                    cursor,
+                    estado_atual,
+                    f"Cadeia rejeitada. Sem transições disponíveis no estado {estado_atual}",
+                )
+                return False
 
-            if len(cadeia) == 0:
-                return estado_atual in self.estados_finais
+            estado_atual = self.funcao_de_transicao[estado_atual][char]
+            cursor += 1
 
-        return False
+        aceita = estado_atual in self.estados_finais
+        self.__print_config(
+            cadeia, cursor, estado_atual, "Aceita" if aceita else "Rejeitada"
+        )
+        return aceita
+
+    def __print_config(self, cadeia: str, cursor: int, estado: Estado, msg: str = ""):
+        if not self.verbose:
+            return
+
+        # Clear the previous output
+
+        output_lines = [
+            f"Cadeia:   {cadeia}",
+            f"{'|'.rjust(cursor + 12)}",
+            f"Caractere: {(cadeia[cursor] if cursor < len(cadeia) else '').rjust(cursor + 1)}",
+            f"Estado: {estado}",
+            f"Inicial: {'Sim' if estado == self.estado_inicial else 'Não'}",
+            f"Final: {'Sim' if estado in self.estados_finais else 'Não'}",
+            msg,
+        ]
+
+        # Print the new output
+        for line in output_lines:
+            print(line)
+
+        if self.interactive:
+            input()
+            sys.stdout.write("\033[F" * 8)  # Move the cursor up 6 lines
+            sys.stdout.write("\033[K" * 8)  # Clear the line
 
     def __checa_eh_valido(self) -> None:
+        pass
         checa_conjunto_vazio(self.alfabeto, "Alfabeto")
         checa_eh_subconjunto(
             self.estados, "Estados", self.estados_finais, "Estados finais"
@@ -59,6 +100,6 @@ class AutomatoFinitoDeterministico:
         checa_eh_subconjunto(
             self.alfabeto,
             "Alfabeto",
-            set(map(lambda t: cast(Transicao, t).simbolo, self.funcao_de_transicao)),
+            set(map(lambda t: list(t.keys())[0], self.funcao_de_transicao.values())),
             "transições",
         )
--- exemplo_automato_finito.py
+++ exemplo_automato_finito.py
@@ -1,4 +1,4 @@
-from automato_finito import AutomatoFinitoDeterministico, Transicao
+from automato_finito import AutomatoFinitoDeterministico
 from enum import Enum
 
 Estados = Enum("Estados", ["q0", "q1", "q2", "q3"])
@@ -8,21 +8,21 @@ adf: AutomatoFinitoDeterministico = AutomatoFinitoDeterministico(
     set(Estados),
     {"a", "b"},
     {
-        Transicao(Estados.q0, "a", Estados.q1),
-        Transicao(Estados.q1, "a", Estados.q1),
-        Transicao(Estados.q1, "b", Estados.q2),
-        Transicao(Estados.q2, "a", Estados.q3),
-        Transicao(Estados.q2, "b", Estados.q2),
-        Transicao(Estados.q3, "a", Estados.q1),
-        Transicao(Estados.q3, "b", Estados.q2),
+        Estados.q0: {"a": Estados.q1},
+        Estados.q1: {"a": Estados.q1, "b": Estados.q2},
+        Estados.q2: {"a": Estados.q3, "b": Estados.q2},
+        Estados.q3: {"a": Estados.q1, "b": Estados.q2},
     },
     Estados.q0,
     {Estados.q3},
 )
 
-print(adf.valida_cadeia("aaaabbcabbbcacc", verbose=True), end="\n\n")
-print(adf.valida_cadeia("aaaaaaaba", verbose=True), end="\n\n")
-print(adf.valida_cadeia("aba", verbose=True), end="\n\n")
-print(adf.valida_cadeia("ba", verbose=True), end="\n\n")
-print(adf.valida_cadeia("ab", verbose=True), end="\n\n")
-print(adf.valida_cadeia("abbabababa", verbose=True), end="\n\n")
+adf.verbose = True
+adf.interactive = True
+
+print(adf.valida_cadeia("aaaabbcabbbcacc"), end="\n\n")
+# print(adf.valida_cadeia("aaaaaaaba"), end="\n\n")
+# print(adf.valida_cadeia("aba"), end="\n\n")
+# print(adf.valida_cadeia("ba"), end="\n\n")
+# print(adf.valida_cadeia("ab"), end="\n\n")
+# print(adf.valida_cadeia("abbabababa"), end="\n\n")
